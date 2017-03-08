4d0d382a8b7656281cc981279a693a6d02d8d6c4
apcnc <fchluca@web.de>
2017-03-08 20:43:55 +0100

    healthbar position

---
+++ Assets/Healthbarpositioning.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbarpositioning : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector3 wantedPos = Camera.main.WorldToViewportPoint (gameObject.transform.position);
+		//transform.position = wantedPos;
+		//print (wantedPos);
+	}
+}
