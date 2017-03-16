82dbb377c0b29a39bcfaae34bc1d956b8bd87d25
apcnc <fchluca@web.de>
2017-03-16 21:41:24 +0100

    enenmyspawn

---
+++ Assets/Scripts/Enemyspawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyspawn : MonoBehaviour {
+
+	public GameObject[] spawnableEnemy;
+	private Transform[] spawnpoints;
+	int i=0;
+	// Use this for initialization
+	void Start () {
+		spawnpoints = gameObject.GetComponentsInChildren<Transform> ();
+		while (i < spawnpoints.Length) 
+		{
+			print(spawnpoints [i].position);
+			i++;
+		}
+	}
+
+}
