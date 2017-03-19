e86830068934bb6e7875546ecec48bd280fb51ac
apcnc <fchluca@web.de>
2017-03-19 16:25:21 +0100

    EnemySpawnPanel v1

--- Assets/Scripts/Enemyspawn.cs
+++ Assets/Scripts/Enemyspawn.cs
@@ -5,15 +5,56 @@ using UnityEngine;
 public class Enemyspawn : MonoBehaviour {
 
 	public GameObject[] spawnableEnemy;
+	private int spawnedEnemys;
+	private int alreadySpawnedEnemys;
+	private int playerLVL;
+	private int currentSpawnPosition;
+	private bool[] alreadyUsedPositions;
+	private GameObject currentlySpawnEnemy;
 	private Transform[] spawnpoints;
-	int i=0;
+
 	// Use this for initialization
 	void Start () {
 		spawnpoints = gameObject.GetComponentsInChildren<Transform> ();
-		while (i < spawnpoints.Length) 
+		alreadyUsedPositions = new bool[spawnpoints.Length];
+
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		
+		if (other.transform.tag == "Player") 
 		{
-			print(spawnpoints [i].position);
-			i++;
+			playerLVL = other.transform.root.GetComponent<CharacterStats> ().lvlheaven + other.transform.root.GetComponent<CharacterStats> ().lvlhell;
+			if (playerLVL < 10) 
+			{
+				spawnedEnemys = (spawnpoints.Length/2);
+			}
+			if (playerLVL >= 10 && playerLVL < 25) 
+			{
+				spawnedEnemys = (spawnpoints.Length/5);
+			}
+			if (playerLVL >= 25 && playerLVL < 50) 
+			{
+				spawnedEnemys = (spawnpoints.Length/4);
+			}
+			if (playerLVL >= 50 && playerLVL < 100) 
+			{
+				spawnedEnemys = (spawnpoints.Length/3);
+			}
+			while(spawnedEnemys>alreadySpawnedEnemys)
+			{
+				currentlySpawnEnemy = spawnableEnemy[Random.Range(0,spawnableEnemy.Length)];
+				currentSpawnPosition = Random.Range (0,spawnpoints.Length);
+				if (!alreadyUsedPositions [currentSpawnPosition]) 
+				{
+					print (spawnpoints [currentSpawnPosition].position);
+					Instantiate (currentlySpawnEnemy,spawnpoints[currentSpawnPosition].position,spawnpoints[currentSpawnPosition].rotation);
+					alreadyUsedPositions [currentSpawnPosition] = true;
+					alreadySpawnedEnemys++;
+				}
+			}
+
+
 		}
 	}
 
